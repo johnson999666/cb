15197bf989945cbd14cd0bc6c99b395f577b643a
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-02-06 15:39:02 -0800

    Update decors.py

--- decors.py
+++ decors.py
@@ -1,63 +1,96 @@
-from flask import Flask
+from tkinter import *
+from quizbrain import QuizBrain
 
-app = Flask(__name__)
-
-#Decorators to add a tag around text on web page.
-def make_bold(function):
-    def wrapper():
-        return "<b>" + function() + "</b>"
-    return wrapper
-
-def make_emphasis(function):
-    def wrapper():
-        return "<em>" + function() + "</em>"
-    return wrapper
-
-def make_underlined(function):
-    def wrapper():
-        return "<u>" + function() + "</u>"
-    return wrapper
-
-@app.route('/')
-def hello_world():
-    #Rendering HTML Elements
-    return '<h1 style="text-align: center">Hello, World!</h1>' \
-           '<p>This is a paragraph.</p>' \
-           '<img src="https://media.giphy.com/media/hvS1eKlR75hMr0l7VJ/giphy.gif" width=200>'
+THEME_COLOR = "#375362"
 
 
-#Different routes using the app.route decorator
-@app.route("/bye")
-@make_bold
-@make_emphasis
-@make_underlined
-def bye():
-    return "Bye!"
 
 
-#Creating variable paths and converting the path to a specified data type
-@app.route("/username/<name>/<int:number>")
-def greet(name, number):
-    return f"Hello there {name}, you are {number} years old!"
 
 
-if __name__ == "__main__":
-    #Run the app in debug mode to auto-reload
-    app.run(debug=True)
+class QuizInterface(Label):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title('Quiz Game')
+        self.window.config(padx=50, pady=50, background=THEME_COLOR)
+        self.score_label = Label(text='score = 0', fg='white',background=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+        self.canvas = Canvas(width=300, height=250, bg='white')
+        self.question_text = self.canvas.create_text(150,
+                                                     125,
+                                                     width=250,
+                                                     text="some text",
+                                                     fill=THEME_COLOR,
+                                                     font=("Arial", 20, "italic"),
+                                                     )
+        self.canvas.grid(row=1, column=2, columnspan=2, pady=50)
+        true = PhotoImage(file="/Users/james/Desktop/true.png")
+        self.true_button = Button(image=true, highlightthickness=0,command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+        false_button = PhotoImage(file="/Users/james/Desktop/false.png")
+        self.false_button = Button(image=false_button, highlightthickness=0,command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+        self.get_next_question()
 
 
-def logging_decorator(fn):
-    def wrapper(*args, **kwargs):
-        print(f"You called {fn.__name__}{args}")
-        result = fn(args[0], args[1], args[2])
-        print(f"It returned: {result}")
-
-    return wrapper
+        self.window.mainloop()
 
 
-@logging_decorator
-def a_function(a, b, c):
-    return a * b * c
+    def get_next_question(self):
+        self.canvas.config(bg='white')
+        self.score_label.config(text=f"Your score {self.quiz.score}")
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
 
 
-a_function(1, 2, 3)
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg='green')
+        else:
+            self.canvas.config(bg='red')
+        self.window.after(1000, self.get_next_question)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# window = Tk()
+# window.title("Kanye Says...")
+# window.config(padx=50, pady=50)
+#
+# canvas = Canvas(width=400, height=500)
+#
+#
+# question = Label(text='hey there traveler', width=5, height=5)
+# question.grid(row=0, column=0)
+
+
+# true_button = Button(text='helloo', width=5, height=5)
+# true_button.grid(row=2, column=0)
+#
+# false_button = Button(text='helloo', width=5, height=5)
+# false_button.grid(row=2, column=1)
+#
+#
+#
+#
+#
+# window.mainloop()
